15bee6976b3ebf10207e15df4e0cf35d2081ed4c
umdoyuun <tkdtlr1998@naver.com>
2023-11-03 17:36:35 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import subprocess
+import os
+import signal
+import sys
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+stream_process = None
+
+def signal_handler(sig, frame):
+    print('all process stop')
+    global stream_process
+    if stream_process is not None and stream_process.poll() is None:
+        stream_process.terminate()
+    sys.exit(0)
+
+@app.route('/start', methods=['GET'])
+def start_stream():
+    global stream_process
+    if stream_process is None or stream_process.poll() is not None:
+        stream_process = subprocess.Popen(["python", "streaming.py"])
+        return "streaming started"
+    else:
+        return "streaming is already running"
+
+@app.route('/stop', methods=['GET'])
+def stop_stream():
+    global stream_process
+    if stream_process is not None and stream_process.poll() is None:
+        stream_process.terminate()
+        return "streaming stopped"
+    else:
+        return "Face recognition is not running"
+
+signal.signal(signal.SIGINT, signal_handler)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9999)
